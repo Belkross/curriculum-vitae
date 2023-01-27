73f45ea2df6bb3d4ea810951a5b9cf829aaa5fcc
Belkross <belkross.pro@gmail.com>
2023-01-27 21:04:38 +0100

    improve content

--- src/components/profile-description.tsx
+++ src/components/profile-description.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import fullfillNonDefaultLanguageFields from "../functions/fulfill-non-default-language-fields"
 import palette from "../theme/palette"
-import { PageContent } from "../types"
+import { PageContent } from "../types/types"
 import { useLanguage } from "./provider-language"
 
 const content: PageContent = fullfillNonDefaultLanguageFields({
@@ -11,8 +11,8 @@ const content: PageContent = fullfillNonDefaultLanguageFields({
     en: "Profile",
   },
   description: {
-    fr: "Je pratique la programmation depuis maintenant plus d’un an dans le but d’une reconversion dans le développement web. J’ai construit mon expérience autour du langage Javascript avec des projets personnels complets, incluant du contenu backend et frontend. Je suis fasciné par les possibilités de réalisation, de partage que permet le code et c’est pour cette raison que développement web est devenu plus qu’un objectif de carrière. C’est maintenant un rendez-vous journalier.",
-    en: "I have been learning to code for more than a year now with the project of retraining in web development. I built my experience around the Javascript language with complete personal projects, including backend and frontend content. I’m fascinated by the possibilities of realization, of sharing that the code allows and this is the reason why web development has become more than a career objective. It is now a daily pastime.",
+    fr: "Je pratique la programmation depuis maintenant deux ans dans le but d’une reconversion dans le développement web. J’ai construit mon expérience autour du langage Javascript avec des projets personnels complets, incluant du contenu backend et frontend. Je suis fasciné par les possibilités de réalisation, de partage que permet le code et c’est pour cette raison que développement web est devenu plus qu’un objectif de carrière. C’est maintenant un rendez-vous journalier.",
+    en: "I have been learning to code for two years now with the project of retraining in web development. I built my experience around the Javascript language with complete personal projects, including backend and frontend content. I’m fascinated by the possibilities of realization, of sharing that the code allows and this is the reason why web development has become more than a career objective. It is now a daily pastime.",
   },
   searchingForTitle: {
     fr: "Je recherche: ",
