76186243444adfaf763965f15e60aaaa44502dcb
Belkross <belkross.pro@gmail.com>
2023-01-30 14:40:44 +0100

    feature: SEO, add the description metadata to the page

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -5,9 +5,12 @@ export function Head() {
     <React.Fragment>
       <html lang="fr" />
       <title>Curriculum Vitae</title>
+      <meta name="description" content={description} />
     </React.Fragment>
   )
 }
+
+const description = "Curriculum vitae de TRUONG Huy. Développeur web."
 
 /* TODO: remember the user lang preference and dynamic lang. 
 Each different lang page should be an independant one */
